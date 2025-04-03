86ab9d0cf31e4a59c39c7386a092801686e34564
0haneu1 <0haneu1@yonsei.ac.kr>
2025-04-04 05:33:29 +0900

    edit main

--- fin/main.py
+++ fin/main.py
@@ -20,23 +20,54 @@ print('질병 임베딩 완료')
 patient_symptoms = "우리 아기가 수유 장애가 있고, 귀에 물이 고여요. 말을 하기 힘들어해요."
 patient_emb = embedding_symptoms(patient_symptoms)
 
-# 유사 질병 추천
+# 유사 질병 추천 : top-k 3개로 설정
 best_matches = similarity_symptoms(patient_emb, embedding_csv_path)
 
 top_disease = list(best_matches.keys())[0]
+second_disease = list(best_matches.keys())[1]
+third_disease = list(best_matches.keys())[2]
 
-doctor_info_df, doctor_recommendation_df = load_doctor_data(base_path)
-doctor_info = get_recommended_doctors(
+# 첫번째 추천 질병의 의사 정보 불러오기
+doctor_info_df_1, doctor_recommendation_df_1 = load_doctor_data(base_path)
+doctor_info_1 = get_recommended_doctors(
     top_disease,
+    os.path.join(base_path, "doctor_recommendation.csv"),
+    os.path.join(base_path, "doctor_info.csv")
+)
+# 두번째 추천 질병의 의사 정보 불러오기
+doctor_info_df_2, doctor_recommendation_df_2 = load_doctor_data(base_path)
+doctor_info_2 = get_recommended_doctors(
+    second_disease,
+    os.path.join(base_path, "doctor_recommendation.csv"),
+    os.path.join(base_path, "doctor_info.csv")
+)
+# 세번째 추천 질병의 의사 정보 불러오기
+doctor_info_df_3, doctor_recommendation_df_3 = load_doctor_data(base_path)
+doctor_info_3 = get_recommended_doctors(
+    third_disease,
     os.path.join(base_path, "doctor_recommendation.csv"),
     os.path.join(base_path, "doctor_info.csv")
 )
 
 # 증상 요약
-summary = summarize_symptoms(patient_symptoms, top_disease)
+summary_1 = summarize_symptoms(patient_symptoms)
+summary_2 = summarize_symptoms(patient_symptoms)
+summary_3 = summarize_symptoms(patient_symptoms)
 
 # 최종 출력
 print(f"환자 증상: {patient_symptoms}")
+print(f"환자 증상 요약: {summarize_symptoms(patient_symptoms)}")
+print()
 print(f"가장 유사한 질병: {top_disease}")
-print(f"증상 요약: {summary}")
-print(f"추천 의사 정보: {doctor_info}")
+print(f"증상 요약: {summary_1}")
+print(f"추천 의사 정보: {doctor_info_1}")
+print()
+print(f"두번째로 유사한 질병: {second_disease}")
+print(f"증상 요약: {summary_2}")
+print(f"추천 의사 정보: {doctor_info_2}")
+print()
+print(f"가장 유사한 질병: {third_disease}")
+print(f"증상 요약: {summary_3}")
+print(f"추천 의사 정보: {doctor_info_3}")
+print()
+
--- fin/summarization.py
+++ fin/summarization.py
@@ -3,9 +3,9 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain.chat_models import ChatOpenAI
 from langchain_community.chat_models import ChatOpenAI
 
-def summarize_symptoms(disease_symptoms, recommended_disease):
+def summarize_symptoms(disease_symptoms):
     docs = [Document(page_content=disease_symptoms)]
-    model = ChatOpenAI(model="gpt-4o")
+    model = ChatOpenAI(model="gpt-4o-mini")
     chain = load_summarize_chain(model, chain_type="map_reduce")
     summary = chain.run(docs)
     return summary
