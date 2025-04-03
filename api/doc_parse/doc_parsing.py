044baf861515d0da0868dcea4f7e731d8b19f72e
mjxjung <mjjung23@yonsei.ac.kr>
2025-04-03 16:51:10 +0900

    doc parsing api 일괄 부부탁

--- api/doc_parse/doc_parsing.py
+++ api/doc_parse/doc_parsing.py
@@ -5,11 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
-PDF_PATH = "../../pdf_samples/구강_구개열_crop.pdf"
-OUTPUT_JSON = "../output/구강_구개열_crop_parsing.json"
-OUTPUT_TEXT = "../output/구강_구개열_crop_parsing.txt"
+PDF_PATH = "../../Database/diseaseinfo/구강_crop/구강_구내염.pdf"
+OUTPUT_JSON = "../../Database/input/구강_구내염_crop_parsing.json"
 
-def call_document_parse_text_only(input_file, json_output_file, text_output_file):
+def call_document_parse_text_only(input_file, json_output_file):
     response = requests.post(
         "https://api.upstage.ai/v1/document-digitization",
         headers={"Authorization": f"Bearer {API_KEY}"},
@@ -30,14 +29,13 @@ def call_document_parse_text_only(input_file, json_output_file, text_output_file
             page_text = page.get("html", "")
             full_text += page_text.strip() + "\n\n"
 
-        with open(text_output_file, "w", encoding="utf-8") as f_text:
-            f_text.write(full_text)
+        
 
-        print(f"✅ 텍스트 추출 완료: {text_output_file}")
+        print(f"✅ json 추출 완료: {json_output_file}")
 
     else:
         raise ValueError(f"[ERROR] status {response.status_code}: {response.text}")
 
 
 # 실행
-call_document_parse_text_only(PDF_PATH, OUTPUT_JSON, OUTPUT_TEXT)
+call_document_parse_text_only(PDF_PATH, OUTPUT_JSON)
